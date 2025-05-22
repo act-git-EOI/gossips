259aa205a6475890e755d98f46cc4dd0312423e8
ESA <estela@gmail.com>
2025-05-22 15:36:38 +0200

    config añadida: iker

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 firebase.initializeApp({
-    apiKey: "XXXXXXXXXXXXXXXXXXXX",
-    authDomain: "XXXXXXXXXXXXXXXXXXXX",
-    projectId: "XXXXXXXXXXXXXXXXXXXX",
-    storageBucket: "XXXXXXXXXXXXXXXXXXXX",
-    messagingSenderId: "XXXXXXXXXXXXXXXXXXXX",
-    appId: "XXXXXXXXXXXXXXXXXXXX"
+    apiKey: "AIzaSyB_zAbsZRncrgvKsQwz_Poo9zWT0JG1938",
+    authDomain: "gossipis.firebaseapp.com",
+    projectId: "gossipis",
+    storageBucket: "gossipis.firebasestorage.app",
+    messagingSenderId: "1023394385289",
+    appId: "1:1023394385289:web:6902f6ba676639524a2558"
 });
 
 const auth = firebase.auth();
